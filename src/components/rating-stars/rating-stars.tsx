cfb854a6bec0937ab0da555afa6a5271451181da
Kangooboy <kangooboy@gmail.com>
2022-10-19 17:01:53 +0300

    fix socials and star color

--- src/components/rating-stars/rating-stars.tsx
+++ src/components/rating-stars/rating-stars.tsx
@@ -8,7 +8,7 @@ export default function RatingStars({rating}: Props): JSX.Element {
     <>
       {Array.from(Array(STARS_COUNT), (_, index) => (
         <img src={(index < rating) ?
-          '/img/sprite/icon-full-star.svg' : '/img/sprite/icon-star.svg'}
+          '/img/sprite/icon-full-star.svg' : '/img/svg/star-disabled.svg'}
         alt="star-rating icon" width="17" height="16" aria-hidden="true" key={index} data-testid="stars"
         />))}
     </>);
